cd38e164a3d3413687de03977085057e65782717
Adunovskiy <71833355+Adunovskiy@users.noreply.github.com>
2020-10-20 17:38:01 +0200

    Add files via upload

---
+++ lab3_5.py
@@ -0,0 +1,14 @@
+import numpy as np
+N = int(input("Введите высоту: "))
+M = int(input("Введите ширину: "))
+A = np.ndarray(shape=(N, M))
+for i in range(N):
+    for j in range(M):
+        if i == 1 or j == 1:
+            A[i, j] = np.sin(N * i + M * j)
+        else:
+            A[i, j] = np.sin(N * (i + 1) + M * (j + 1))
+        if A[i, j] < 0:
+            A[i, j] = 0
+A = A[:, :: -1]            
+print(A)
